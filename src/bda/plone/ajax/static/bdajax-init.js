aa932619978ee4f1af63312f2902600ec25f4d65
Peter Holzer <hpeter@agitator.com>
2016-03-01 12:34:53 +0100

    added bdajax init

---
+++ src/bda/plone/ajax/static/bdajax-init.js
@@ -0,0 +1,35 @@
+/*jslint browser: true*/
+/*global $, jQuery, define*/
+
+/* global require */
+
+if(window.jQuery){
+  define('jquery', function(){
+    return window.jQuery;
+  });
+}
+
+
+require([
+  'jquery',
+  'bdajax',
+  ], function($,
+              bdajax
+              ){
+  'use strict';
+
+  $(document).ready(function(){
+
+    // XXX this mus be wrapped in a document ready
+    bdajax.spinner.hide();
+    $(document).bdajax();
+    // /XXX
+
+
+
+  });
+
+
+
+
+});
